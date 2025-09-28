b9fd37c62948dc08ee022fea7b1446852c82f9d9
Yuwhyy <lemasia71@gmail.com>
2025-09-28 18:27:48 +0700

    Add files via upload

---
+++ Apoteknew.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Obat {
+    string nama;
+    int stok;
+    int batasMinimum;
+};
+
+int main() {
+    vector<Obat> daftarObat;
+    string namaObat;
+    int stok, batasMin = 10; 
+    char lanjut;
+
+    cout << "=== PROGRAM APOTEK - INPUT STOK OBAT ===" << endl;
+
+    do {
+        cout << "\nMasukkan nama obat: ";
+        getline(cin, namaObat);
+
+        cout << "Masukkan stok obat: ";
+        cin >> stok;
+
+        
+        Obat o;
+        o.nama = namaObat;
+        o.stok = stok;
+        o.batasMinimum = batasMin;
+        daftarObat.push_back(o);
+
+        cin.ignore(); 
+
+        cout << "Tambah obat lagi? (y/n): ";
+        cin >> lanjut;
+        cin.ignore();
+
+    } while (lanjut == 'y' || lanjut == 'Y');
+
+    
+    cout << "\n=== DAFTAR STOK OBAT ===" << endl;
+    for (const auto& o : daftarObat) {
+        cout << "- " << o.nama 
+             << " | Stok: " << o.stok 
+             << " | Batas Minimum: " << o.batasMinimum;
+        
+        if (o.stok < o.batasMinimum) {
+            cout << " STOK RENDAH!";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
